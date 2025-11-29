e5e8c236f6bcfca98367a5065d7d348e93fb29c8
Rupen Gotmare <rupengotmare@gmail.com>
2025-11-30 02:05:11 +0530

    new changes added

--- revi1.sh
+++ revi1.sh
@@ -1,69 +1,84 @@
-# Topics in Linux revisions:-
+Topics in Linux revisions:-
 
-# November 28 :- 
-# Root user - #
-# Normal user/local user - $
-# Directory - /
+November 28 :- 
+Root user - #
+Normal user/local user - $
+Directory - /
 
-# read operations :- cat, head, tail, less, more, wc, sort, uniq, cut, grep, find, diff, cmp, file, strings
-# write operations :- echo, printf, tee, touch, mv, cp, rm, rmdir, mkdir, rename
-# permission operations :- chmod, chown, chgrp, umask, su, sudo
-# process operations :- ps, top, htop, kill, killall, pkill, nice, renice, jobs, fg, bg, wait
-# network operations :- ping, ifconfig, netstat, ss, traceroute, curl, wget, ftp, scp, ssh
-# system operations :- uname, df, du, free, uptime, dmesg, lsof, lsmod, modprobe, mount, umount
-# package management :- apt, yum, dnf, pacman, zypper
-# file system operations :- fsck, mkfs, tune2fs, mount, umount, blkid, lsblk
-# user management :- useradd, usermod, userdel, groupadd, groupmod, groupdel, passwd  
+read operations :- cat, head, tail, less, more, wc, sort, uniq, cut, grep, find, diff, cmp, file, strings
+write operations :- echo, printf, tee, touch, mv, cp, rm, rmdir, mkdir, rename
+permission operations :- chmod, chown, chgrp, umask, su, sudo
+process operations :- ps, top, htop, kill, killall, pkill, nice, renice, jobs, fg, bg, wait
+network operations :- ping, ifconfig, netstat, ss, traceroute, curl, wget, ftp, scp, ssh
+system operations :- uname, df, du, free, uptime, dmesg, lsof, lsmod, modprobe, mount, umount
+package management :- apt, yum, dnf, pacman, zypper
+file system operations :- fsck, mkfs, tune2fs, mount, umount, blkid, lsblk
+user management :- useradd, usermod, userdel, groupadd, groupmod, groupdel, passwd  
 
-# Linux directory structure :-
-# 1>/ - Root directory
-# 2>/bin - Essential command binaries
-# 3>/boot - Boot loader files
-# 4>/dev - Device files
-# 5>/etc - System configuration files
-# 6>/home - User home directories
-# 7>/lib - Essential shared libraries
-# 8>/media - Removable media
-# 9>/mnt - Temporary mount points
-# 10>/opt - Optional software packages
-# 11>/proc - Process information
-# 12>/root - Root user's home directory
-# 13>/sbin - System binaries
-# 14>/tmp - Temporary files
-# 15>/usr - User utilities and applications
-# 16>/var - Variable data files 
+Linux directory structure :-
+1>/ - Root directory
+2>/bin - Essential command binaries
+3>/boot - Boot loader files
+4>/dev - Device files
+5>/etc - System configuration files
+6>/home - User home directories
+7>/lib - Essential shared libraries
+8>/media - Removable media
+9>/mnt - Temporary mount points
+10>/opt - Optional software packages
+11>/proc - Process information
+12>/root - Root users home directory
+13>/sbin - System binaries
+14>/tmp - Temporary files
+15>/usr - User utilities and applications
+16>/var - Variable data files 
 
-# Basic Linux commands :-
-# ls, cd, pwd, mkdir, rmdir, touch, cp, mv, rm, cat, less, more, head, tail, grep, find, chmod, chown, ps, top, kill, ping, ifconfig, uname, df, du, free, uptime,
-# man, echo, printf, tee, sort, uniq, cut, diff, cmp, file, strings, wc, jobs, fg, bg, wait, htop, killall, pkill, nice, renice, traceroute, curl, wget, ftp, scp, ssh,
-# apt, yum, dnf, pacman, zypper, fsck, mkfs, tune2fs, blkid, lsblk, useradd, usermod, userdel, groupadd, groupmod, groupdel, passwd
+Basic Linux commands :-
+ls, cd, pwd, mkdir, rmdir, touch, cp, mv, rm, cat, less, more, head, tail, grep, find, chmod, chown, ps, top, kill, ping, ifconfig, uname, df, du, free, uptime,
+man, echo, printf, tee, sort, uniq, cut, diff, cmp, file, strings, wc, jobs, fg, bg, wait, htop, killall, pkill, nice, renice, traceroute, curl, wget, ftp, scp, ssh,
+apt, yum, dnf, pacman, zypper, fsck, mkfs, tune2fs, blkid, lsblk, useradd, usermod, userdel, groupadd, groupmod, groupdel, passwd
 
-# File permissions :-
-# r - read permission
-# w - write permission
-# x - execute permission
-# - - no permission
+File permissions :-
+r - read permission
+w - write permission
+x - execute permission
+- - no permission
 
-# Editors :- 
-# vim, nano, emacs, gedit, sed, awk 
-# (Difference between vi and vim: vim is an improved version of vi with additional features like syntax highlighting, multi-level undo, and a more user-friendly interface.)
-# (Difference between nano and vim: nano is a simple, user-friendly text editor suitable for beginners, while vim is a more powerful and complex editor with a steeper learning curve.)
+Editors :- 
+vim, nano, emacs, gedit, sed, awk 
+(Difference between vi and vim: vim is an improved version of vi with additional features like syntax highlighting, multi-level undo, and a more user-friendly interface.)
+(Difference between nano and vim: nano is a simple, user-friendly text editor suitable for beginners, while vim is a more powerful and complex editor with a steeper learning curve.)
 
-# Vim commands :-
-# i - insert mode
-# Esc - command mode
-# :w - save file
-# :q - quit
-# :wq - save and quit
-# :q! - quit without saving
+Vim commands :-
+i - insert mode
+Esc - command mode
+:w - save file
+:q - quit
+:wq - save and quit
+:q! - quit without saving
 
-# dd - delete line
-# yy - copy line
-# p - paste line
-# u - undo
-# Ctrl + r - redo
-# /: - search
-# :%s/old/new/g - replace all occurrences of 'old' with 'new'
-# :set nu - show line numbers
-# :set nonu - hide line numbers
+dd - delete line
+yy - copy line
+p - paste line
+u - undo
+Ctrl + r - redo
+/: - search
+:%s/old/new/g - replace all occurrences of 'old' with 'new'
+:set nu - show line numbers
+:set nonu - hide line numbers
+
+User Permissions :- 
+Local user - umask 002 
+Local users file = 666 - 002 = 664 (rw-rw-r--)
+Local users directory = 777-002 = 775 (rwx-rwx-r-x)
+
+Root user - umask 022
+Root users file = 666 - 022 = 644 (rw-r--r)
+Root users directory = 777 - 022 = 755 (rwxr-xr-x)
+(umask value are the values which tell the system what permission to remove when a new file or directory is created.)
+
+SUID :- this is a special type of permission that allows a user to run an executable with the permissions of the executable's owner. It is represented by an 's' in the owner's execute position (rwsr-xr-x).
+SGID :- this is a special type of permission that allows a user to run an executable with the permissions of the executable's group. It is represented by an 's' in the group's execute position (rwxr-sr-x).
+Sticky bit :- this is a special type of permission that allows only the file owner, the directory owner, or the root user to delete or rename files within a directory. It is represented by a 't' in the others' execute position (rwxr-xr-t).
+To use special permissions, you can use the [___chmod___] command with the following numeric values
 
